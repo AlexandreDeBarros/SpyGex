c7627f26295e9146b208b29257b67e340b18d4da
Alexandre De Barros <45422062+AlexandreDeBarros@users.noreply.github.com>
2023-11-03 15:42:03 +0100

    MAJ MVC

--- src/controller/SpyGexController.py
+++ src/controller/SpyGexController.py
@@ -5,19 +5,11 @@ class SpyGexController:
         self.view = view
 
     def research(self, url):
-        success, visited_url = self.model.get_all_links(url)
-        if success:
-            self.view.show_results(visited_url)
-
-            soup = self.model.url_to_soup(visited_url)
-            regex = r"votre_regex"  
-            matched_content = self.model.match_regex_page(regex, soup)
-            
-            response = requests.get(visited_url)
-            soup = BeautifulSoup(response.content, 'html.parser')
-            links = soup.find_all('a')
-
-            for link in links:
-                href = link.get('href')
-                if href and href.startswith('http'):
-                    self.research(href)
+        try:
+            self.model.get_all_links(url, regex)
+            self.view.show_results(self.model.visited_urls)
+            self.view.show_matched_content(self.model.matches)
+        except requests.RequestException as e:
+            print(f"Erreur de requête HTTP pour l'URL {url}: {e}")
+        except Exception as e:
+            print(f"Erreur lors du traitement de l'URL {url}: {e}")
--- src/model/SpyGexModel.py
+++ src/model/SpyGexModel.py
@@ -5,14 +5,14 @@ import requests
 
 class SpyGexModel:
 
-    def __init__(self,regex,url,matched_results) :
+    def __init__(self,regex,url) :
         self.regex = regex
         self.url = url
-        self.matched_results = matched_results
+        self.matched_results
         self.visited_urls = set()
 
-    def match_regex_page(self, regex, soup):
-        matched_content = soup.find_all(string=re.compile(regex))
+    def match_regex_page(self, soup):
+        matched_content = soup.find_all(string=re.compile(self.regex))
         self.matched_results.extend(matched_content)
         return matched_content
 
@@ -21,6 +21,23 @@ class SpyGexModel:
     
     def is_same_domain(self, url):
         return urlparse(url).netloc == urlparse(self.url).netloc
+
+    def get_all_links():
+        success, visited_url = self.model.get_all_links(url)
+        if success:
+            self.view.show_results(visited_url)
+
+            soup = self.model.url_to_soup(visited_url)
+            matched_content = self.model.match_regex_page(self.regex, soup)
+            
+            response = requests.get(visited_url)
+            soup = BeautifulSoup(response.content, 'html.parser')
+            links = soup.find_all('a')
+
+            for link in links:
+                href = link.get('href')
+                if href and href.startswith('http'):
+                    self.research(href)
     
     def export_csv(self):
         return
