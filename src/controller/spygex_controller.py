ee9ccc8db2e8e595aad277dd9ca15765e57d84cc
Alexandre De Barros <45422062+AlexandreDeBarros@users.noreply.github.com>
2023-11-03 21:03:10 +0100

    MAJ MVC + files name format

--- src/controller/spygex_controller.py
+++ src/controller/spygex_controller.py
@@ -6,14 +6,15 @@ class SpyGexController:
         self.model = model
         self.view = view
 
-    def research(self, url,regex):
+    def research(self):
         try:
-            self.model.get_all_links(url, regex)
+            self.model.get_all_links(self.model.url) 
+            self.model.match_regex_page(self.model.url_to_soup(self.model.url))  
             print(self.model.visited_urls)
             print(self.model.matched_results)
             #self.view.show_results(self.model.visited_urls)
             #self.view.show_matched_results(self.matched_results)
         except requests.RequestException as e:
-            print(f"Erreur de requête HTTP pour l'URL {url}: {e}")
+            print(f"Erreur de requête HTTP pour l'URL : {e}")
         except Exception as e:
-            print(f"Erreur lors du traitement de l'URL {url}: {e}")
+            print(f"Erreur lors du traitement de l'URL : {e}")
--- src/model/spygex_model.py
+++ src/model/spygex_model.py
@@ -5,11 +5,10 @@ import requests
 
 class SpyGexModel:
 
-    regex = ''
-    url = ''
-    matched_results = []
-
-    def __init__(self) :
+    def __init__(self):
+        self.regex = ''
+        self.url = ''
+        self.matched_results = []
         self.visited_urls = set()
 
     def match_regex_page(self, soup):
@@ -17,26 +16,25 @@ class SpyGexModel:
         self.matched_results.extend(matched_content)
         return matched_content
 
-    def url_to_soup(self, url):
+    def url_to_soup(self,url):
         return BeautifulSoup(requests.get(url).content, "html.parser")
-    
-    def is_same_domain(self, url):
-        return urlparse(url).netloc == urlparse(self.url).netloc
 
-    def get_all_links(url):
-        self.visited_urls.add(url)  
+    def is_same_domain(self,url, base_url):
+        return urlparse(url).netloc == urlparse(base_url).netloc
+
+    def get_all_links(self, url):
+        self.visited_urls.add(url)
         print("Visiting:", url)
     
         response = requests.get(url)
-        soup = BeautifulSoup(response.content, 'html.parser')
+        soup = self.url_to_soup(url)
         links = soup.find_all('a')
     
         for link in links:
             href = link.get('href')
-            if href and href.startswith('http') and (href not in visited_urls) and is_same_domain(href):
-                get_all_links(href)  
+            if href and href.startswith('http') and (href not in self.visited_urls) and self.is_same_domain(href, url):
+                self.get_all_links(href)  
     
     def export_csv(self):
+        # Logique d'exportation vers un fichier CSV
         return
-
-    
