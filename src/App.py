b415f9421d1a2fe3fc11d032d0d6f579fbbe57a6
Alexandre De Barros <45422062+AlexandreDeBarros@users.noreply.github.com>
2023-11-05 23:39:15 +0100

    rename import

--- src/App.py
+++ src/App.py
@@ -1,25 +1,25 @@
 import tkinter as tk
 from tkinter import ttk
-from model import SpyGexModel
-from controller import SpyGexController
-from view import SpyGexView
+from model import spygex_model as sm
+from controller import spygex_controller as sc
+from view import spygex_view as sv
 
 class App(tk.Tk):
     def __init__(self):
         super().__init__()
 
         # create the model
-        model = SpyGexModel.SpyGexModel()
+        model = sm.SpyGexModel()
 
         # create the view
-        view = SpyGexView.SpyGexView()
+        view = sv.SpyGexView()
         
         # simulation
         model.regex = "(?:(?:31(\/|-|\.)(?:0?[13578]|1[02]))\1|(?:(?:29|30)(\/|-|\.)(?:0?[13-9]|1[0-2])\2))(?:(?:1[6-9]|[2-9]\d)?\d{2})$|^(?:29(\/|-|\.)0?2\3(?:(?:(?:1[6-9]|[2-9]\d)?(?:0[48]|[2468][048]|[13579][26])|(?:(?:16|[2468][048]|[3579][26])00))))$|^(?:0?[1-9]|1\d|2[0-8])(\/|-|\.)(?:(?:0?[1-9])|(?:1[0-2]))\4(?:(?:1[6-9]|[2-9]\d)?\d{2})|(?:(?:1[6-9]|[2-9]\d)?\d{2})(\/|-|\.)(?:0?[13578]|1[02])(\/|-|\.)31|(?:0?[13-9]|1[0-2])(\/|-|\.)(29|30)$|^(?:(?:(?:1[6-9]|[2-9]\d)?(?:0[48]|[2468][048]|[13579][26])|(?:(?:16|[2468][048]|[3579][26])00)))(\/|-|\.)0?2(\/|-|\.)29$|^(?:(?:1[6-9]|[2-9]\d)?\d{2})(\/|-|\.)(?:(?:0?[1-9])|(?:1[0-2]))(\/|-|\.)(?:0?[1-9]|1\d|2[0-8])$"
         model.url = "https://realpython.github.io/fake-jobs/"
 
         # create a controller
-        controller = SpyGexController.SpyGexController(model, view)
+        controller = sc.SpyGexController(model, view)
         controller.research()
 
 
